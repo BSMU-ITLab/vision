46455eb3d0f132fbb5bd6426e24f29f1c0f02da4
Ivan Kosik <ivankosik91@gmail.com>
2023-01-25 14:03:49 +0300

    Improved image writer plugin to support save and save as operations.

--- vision-plugins/src/bsmu/vision/plugins/writers/image/generic.py
+++ vision-plugins/src/bsmu/vision/plugins/writers/image/generic.py
@@ -37,18 +37,19 @@ class GenericImageFileWriterPlugin(FileWriterPlugin):
 
     def _enable_gui(self):
         self._main_window = self._main_window_plugin.main_window
-        self._main_window.add_menu_action(FileMenu, 'Save Mask...', self._select_path_and_save_active_window_image)
+        self._main_window.add_menu_action(FileMenu, 'Save Mask', self._save_active_window_image)
+        self._main_window.add_menu_action(FileMenu, 'Save Mask As...', self._select_path_and_save_active_window_image)
 
         self._mdi = self._mdi_plugin.mdi
 
-    def _select_path_and_save_active_window_image(self):
+    def _active_window_image(self) -> Image | None:
         active_sub_window = self._mdi.activeSubWindow()
         if not isinstance(active_sub_window, LayeredImageViewerHolder):
             QMessageBox.warning(
                 self._main_window,
                 'No Layered Image',
                 'The active window does not contain a layered image.')
-            return
+            return None
 
         layer_name = 'masks'
         image_layer = active_sub_window.layered_image_viewer.layer_by_name(layer_name)
@@ -57,14 +58,47 @@ class GenericImageFileWriterPlugin(FileWriterPlugin):
                 self._main_window,
                 'No Image',
                 f'The layered image does not contain an image in the "{layer_name}" layer.')
+            return None
+
+        return image_layer.image
+
+    def _save_active_window_image(self):
+        image = self._active_window_image()
+        if image is None:
             return
 
+        if image.path is None:
+            self._select_path_and_save_image(image)
+        else:
+            self._save_image(image, image.path)
+
+    def _select_path_and_save_active_window_image(self):
+        image = self._active_window_image()
+        if image is None:
+            return
+
+        self._select_path_and_save_image(image)
+
+    def _select_path_and_save_image(self, image: Image):
         file_name, selected_filter = QFileDialog.getSaveFileName(
             parent=self._main_window, caption='Save Mask', filter='PNG (*.png)')
         if not file_name:
             return
 
-        self._file_writer_cls().write_to_file(image_layer.image, Path(file_name))
+        save_path = Path(file_name)
+        if self._save_image(image, save_path):
+            image.path = save_path
+
+    def _save_image(self, image: Image, path: Path) -> bool:
+        try:
+            self._file_writer_cls().write_to_file(image, path)
+            return True
+        except Exception as e:
+            QMessageBox.warning(
+                self._main_window,
+                'Save Error',
+                f'Cannot save the image.\n{e}')
+            return False
 
 
 class GenericImageFileWriter(FileWriter):

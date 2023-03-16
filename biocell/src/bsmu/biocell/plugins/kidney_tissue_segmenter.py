6d3ab61b03c0d0af87ad608881274e6522bc128a
Ivan Kosik <ivankosik91@gmail.com>
2023-03-16 16:51:56 +0300

    Improved kidney tissue segmenter plugin.

--- biocell/src/bsmu/biocell/plugins/kidney_tissue_segmenter.py
+++ biocell/src/bsmu/biocell/plugins/kidney_tissue_segmenter.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-from timeit import default_timer as timer
 from typing import TYPE_CHECKING
 
 import cv2 as cv
 import numpy as np
 import skimage.color
 from PySide6.QtCore import Qt, QObject
+from PySide6.QtWidgets import QMessageBox
 
 from bsmu.vision.core.converters.image import normalized_uint8
 from bsmu.vision.core.image.base import FlatImage
@@ -19,6 +19,10 @@ if TYPE_CHECKING:
     from bsmu.vision.plugins.windows.main import MainWindowPlugin, MainWindow
     from bsmu.vision.plugins.doc_interfaces.mdi import MdiPlugin, Mdi
     from bsmu.vision.widgets.viewers.image.layered.base import LayeredImageViewer
+
+
+TISSUE_MASK_LAYER_NAME = 'tissue-mask'
+BLUE_MASK_LAYER_NAME = 'blue-mask'
 
 
 class BiocellKidneyTissueSegmenterPlugin(Plugin):
@@ -49,7 +53,8 @@ class BiocellKidneyTissueSegmenterPlugin(Plugin):
         self._main_window = self._main_window_plugin.main_window
         self._mdi = self._mdi_plugin.mdi
 
-        self._main_window.add_menu_action(AlgorithmsMenu, 'Segment Tissue', self._segment_tissue, Qt.Key_0)
+        self._main_window.add_menu_action(AlgorithmsMenu, 'Segment Tissue', self._segment_tissue, Qt.Key_9)
+        self._main_window.add_menu_action(AlgorithmsMenu, 'Analyze Blue Ratio', self._analyze_blue_ratio, Qt.Key_0)
 
     def _disable(self):
         self._mdi_viewer_tool = None
@@ -64,6 +69,14 @@ class BiocellKidneyTissueSegmenterPlugin(Plugin):
 
         layered_image_viewer = layered_image_viewer_sub_window.layered_image_viewer
         self._kidney_tissue_segmenter.segment_tissue(layered_image_viewer)
+
+    def _analyze_blue_ratio(self):
+        layered_image_viewer_sub_window = self._mdi.active_sub_window_with_type(LayeredImageViewerHolder)
+        if layered_image_viewer_sub_window is None:
+            return
+
+        layered_image_viewer = layered_image_viewer_sub_window.layered_image_viewer
+        self._kidney_tissue_segmenter.analyze_blue_ratio(layered_image_viewer)
 
 
 def show_rgb(rgb: np.ndarray, title: str = ''):
@@ -120,42 +133,53 @@ class BiocellKidneyTissueSegmenter(QObject):
         tissue_mask[np.where((stain_0_normalized_uint8 > 1) | (stain_2_normalized_uint8 > 1))] = 1
         tissue_palette = Palette.default_binary(rgb_color=[131, 151, 98])
         layered_image_viewer.data.add_layer_or_modify_pixels(
-            'tissue-mask', tissue_mask, FlatImage, tissue_palette)
+            TISSUE_MASK_LAYER_NAME, tissue_mask, FlatImage, tissue_palette)
 
         blue_mask = np.zeros_like(stain_0_normalized_uint8)
         blue_mask[stain_0_normalized_uint8 > 14] = 1
+        self._analyze_blue_ratio(layered_image_viewer, tissue_mask, blue_mask)
+
+    def analyze_blue_ratio(self, layered_image_viewer: LayeredImageViewer):
+        print('analyze_blue_ratio', type(layered_image_viewer))
+
+        tissue_mask_layer = layered_image_viewer.layer_by_name(TISSUE_MASK_LAYER_NAME)
+        blue_mask_layer = layered_image_viewer.layer_by_name(BLUE_MASK_LAYER_NAME)
+        if not (tissue_mask_layer and blue_mask_layer):
+            QMessageBox.information(layered_image_viewer, 'No Masks', 'Run "Segment Tissue" action first.')
+            return
+        self._analyze_blue_ratio(layered_image_viewer, tissue_mask_layer.image_pixels, blue_mask_layer.image_pixels)
+
+    def _analyze_blue_ratio(
+            self, layered_image_viewer: LayeredImageViewer, tissue_mask: np.ndarray, blue_mask: np.ndarray):
+        # Set the |blue_mask| to zero where tissue mask is zeros (tissue mask can be erased by brash)
+        blue_mask[tissue_mask == 0] = 0
         blue_palette = Palette.default_binary(rgb_color=[255, 179, 19])
         layered_image_viewer.data.add_layer_or_modify_pixels(
-            'blue-mask', blue_mask, FlatImage, blue_palette)
+            BLUE_MASK_LAYER_NAME, blue_mask, FlatImage, blue_palette)
 
         print('\n=== Initial Result ===')
-        self._pixel_count_ratio(blue_mask, tissue_mask)
-
-        print('\n=== Blue Only Within the Tissue ===')
         blue_mask[tissue_mask == 0] = 0
-        self._pixel_count_ratio(blue_mask, tissue_mask)
+        blue_to_tissue_ratio = self._pixel_count_ratio(blue_mask, tissue_mask)
 
         print('\n=== After Erode ===')
         erode_kernel = np.ones((3, 3), np.uint8)
         blue_mask = cv.erode(blue_mask, kernel=erode_kernel, iterations=1)
-        self._pixel_count_ratio(blue_mask, tissue_mask)
+        eroded_blue_to_tissue_ratio = self._pixel_count_ratio(blue_mask, tissue_mask)
 
         blue_eroded_palette = Palette.default_binary(rgb_color=[255, 110, 19])
         layered_image_viewer.data.add_layer_or_modify_pixels(
             'blue-eroded_mask', blue_mask, FlatImage, blue_eroded_palette)
 
-        # st = timer()
-        # tissue_pixel_count = cv.countNonZero(tissue_mask)
-        # blue_pixel_count = cv.countNonZero(blue_mask)
-        # print('cv.countNonZero time: ', timer() - st)
-        # blue_to_tissue_ratio = blue_pixel_count / tissue_pixel_count
-        # print(f'tissue_pixel_count: {tissue_pixel_count}  blue_pixel_count: {blue_pixel_count}  '
-        #       f'ratio: {blue_to_tissue_ratio}')
+        ratio_message = f'Blue: {self._float_to_persent_str(blue_to_tissue_ratio)}' \
+                        f'\nEroded Blue: {self._float_to_persent_str(eroded_blue_to_tissue_ratio)}'
+        QMessageBox.information(layered_image_viewer, 'Blue Ratio', ratio_message)
 
-    def _pixel_count_ratio(self, a: np.ndarray, b: np.ndarray):
-        st = timer()
+    def _pixel_count_ratio(self, a: np.ndarray, b: np.ndarray) -> float:
         a_pixel_count = cv.countNonZero(a)
         b_pixel_count = cv.countNonZero(b)
-        print('cv.countNonZero time: ', timer() - st)
         a_to_b_ratio = a_pixel_count / b_pixel_count
         print(f'a_pixel_count: {a_pixel_count}  b_pixel_count: {b_pixel_count}  ratio: {a_to_b_ratio}')
+        return a_to_b_ratio
+
+    def _float_to_persent_str(self, value: float) -> str:
+        return f'{value * 100:.2f}%'

3af80d9f979885c8c197f26d6014f403e43a729f
Ivan Kosik <ivankosik91@gmail.com>
2024-03-19 15:09:20 +0300

    Changed kidney segmenter to combine all masks into one. Mask saving is working now.

--- biocell/src/bsmu/biocell/plugins/kidney_tissue_segmenter.py
+++ biocell/src/bsmu/biocell/plugins/kidney_tissue_segmenter.py
@@ -19,23 +19,26 @@ from bsmu.vision.widgets.viewers.image.layered.base import LayeredImageViewerHol
 if TYPE_CHECKING:
     from bsmu.vision.plugins.windows.main import MainWindowPlugin, MainWindow
     from bsmu.vision.plugins.doc_interfaces.mdi import MdiPlugin, Mdi
+    from bsmu.vision.plugins.palette.settings import PalettePackSettingsPlugin, PalettePackSettings
     from bsmu.vision.widgets.viewers.image.layered.base import LayeredImageViewer
 
 
-TISSUE_MASK_LAYER_NAME = 'tissue-mask'
-BLUE_MASK_LAYER_NAME = 'blue-mask'
+MASKS_LAYER_NAME = 'masks'
+SEGMENT_TISSUE_ACTION_NAME = 'Segment Tissue - Stain Separation'
 
 
 class BiocellKidneyTissueSegmenterPlugin(Plugin):
     _DEFAULT_DEPENDENCY_PLUGIN_FULL_NAME_BY_KEY = {
         'main_window_plugin': 'bsmu.vision.plugins.windows.main.MainWindowPlugin',
         'mdi_plugin': 'bsmu.vision.plugins.doc_interfaces.mdi.MdiPlugin',
+        'palette_pack_settings_plugin': 'bsmu.vision.plugins.palette.settings.PalettePackSettingsPlugin',
     }
 
     def __init__(
             self,
             main_window_plugin: MainWindowPlugin,
             mdi_plugin: MdiPlugin,
+            palette_pack_settings_plugin: PalettePackSettingsPlugin,
     ):
         super().__init__()
 
@@ -45,17 +48,20 @@ class BiocellKidneyTissueSegmenterPlugin(Plugin):
         self._mdi_plugin = mdi_plugin
         self._mdi: Mdi | None = None
 
+        self._palette_pack_settings_plugin = palette_pack_settings_plugin
+
         self._kidney_tissue_segmenter: BiocellKidneyTissueSegmenter | None = None
 
     def _enable(self):
-        self._kidney_tissue_segmenter = BiocellKidneyTissueSegmenter()
+        main_palette = self._palette_pack_settings_plugin.settings.main_palette
+        self._kidney_tissue_segmenter = BiocellKidneyTissueSegmenter(main_palette)
 
     def _enable_gui(self):
         self._main_window = self._main_window_plugin.main_window
         self._mdi = self._mdi_plugin.mdi
 
-        self._main_window.add_menu_action(AlgorithmsMenu, 'Segment Tissue - Stain Separation', self._segment_tissue, Qt.Key_9)
-        self._main_window.add_menu_action(AlgorithmsMenu, 'Segment Tissue - Saturation Threshold', self._segment_tissue_using_saturation_threshold, Qt.Key_7)
+        self._main_window.add_menu_action(AlgorithmsMenu, SEGMENT_TISSUE_ACTION_NAME, self._segment_tissue, Qt.Key_9)
+        # self._main_window.add_menu_action(AlgorithmsMenu, 'Segment Tissue - Saturation Threshold', self._segment_tissue_using_saturation_threshold, Qt.Key_7)
         self._main_window.add_menu_action(AlgorithmsMenu, 'Analyze Blue Ratio', self._analyze_blue_ratio, Qt.Key_0)
 
     def _disable(self):
@@ -96,8 +102,14 @@ def show_rgb(rgb: np.ndarray, title: str = ''):
 
 
 class BiocellKidneyTissueSegmenter(QObject):
-    def __init__(self):
+    def __init__(self, mask_palette: Palette):
         super().__init__()
+
+        self._mask_palette = mask_palette
+        self._mask_background_class = mask_palette.row_index_by_name('background')
+        self._mask_foreground_class = mask_palette.row_index_by_name('foreground')
+        self._mask_blue_class = mask_palette.row_index_by_name('blue')
+        self._mask_eroded_blue_class = mask_palette.row_index_by_name('eroded_blue')
 
     def segment_tissue(self, layered_image_viewer: LayeredImageViewer):
         print('segment_tissue', type(layered_image_viewer))
@@ -139,46 +151,44 @@ class BiocellKidneyTissueSegmenter(QObject):
         stain_1_normalized_uint8 = normalized_uint8(stain_1)
         stain_2_normalized_uint8 = normalized_uint8(stain_2)
 
-        tissue_mask = np.zeros_like(stain_0_normalized_uint8)
-        tissue_mask[np.where((stain_0_normalized_uint8 > 1) | (stain_2_normalized_uint8 > 7))] = 1
-        tissue_palette = Palette.default_binary(rgb_color=[131, 151, 98])
-        layered_image_viewer.data.add_layer_or_modify_pixels(
-            TISSUE_MASK_LAYER_NAME, tissue_mask, FlatImage, tissue_palette)
+        mask = np.zeros_like(stain_0_normalized_uint8)
+        mask[(stain_0_normalized_uint8 > 1) | (stain_2_normalized_uint8 > 7)] = self._mask_foreground_class
 
-        blue_mask = np.zeros_like(stain_0_normalized_uint8)
-        blue_mask[stain_0_normalized_uint8 > 14] = 1
-        self._analyze_blue_ratio(layered_image_viewer, tissue_mask, blue_mask)
+        is_blue_mask = stain_0_normalized_uint8 > 14
+        mask[is_blue_mask] = self._mask_blue_class
+
+        erode_kernel = np.ones((3, 3), np.uint8)
+        blue_eroded_mask = cv.erode(is_blue_mask.astype(np.uint8), kernel=erode_kernel, iterations=1)
+        mask[blue_eroded_mask.astype(bool)] = self._mask_eroded_blue_class
+
+        layered_image_viewer.data.add_layer_or_modify_pixels(
+            MASKS_LAYER_NAME, mask, FlatImage, self._mask_palette)
+
+        self._analyze_blue_ratio(layered_image_viewer, mask)
 
     def analyze_blue_ratio(self, layered_image_viewer: LayeredImageViewer):
         print('analyze_blue_ratio', type(layered_image_viewer))
 
-        tissue_mask_layer = layered_image_viewer.layer_by_name(TISSUE_MASK_LAYER_NAME)
-        blue_mask_layer = layered_image_viewer.layer_by_name(BLUE_MASK_LAYER_NAME)
-        if not (tissue_mask_layer and blue_mask_layer):
-            QMessageBox.information(layered_image_viewer, 'No Masks', 'Run "Segment Tissue" action first.')
+        mask_layer = layered_image_viewer.layer_by_name(MASKS_LAYER_NAME)
+        if not mask_layer:
+            QMessageBox.information(
+                layered_image_viewer, 'No Masks', f'Run <b>{SEGMENT_TISSUE_ACTION_NAME}</b> action first.')
             return
-        self._analyze_blue_ratio(layered_image_viewer, tissue_mask_layer.image_pixels, blue_mask_layer.image_pixels)
+        self._analyze_blue_ratio(layered_image_viewer, mask_layer.image_pixels)
 
-    def _analyze_blue_ratio(
-            self, layered_image_viewer: LayeredImageViewer, tissue_mask: np.ndarray, blue_mask: np.ndarray):
-        # Set the |blue_mask| to zero where tissue mask is zeros (tissue mask can be erased by brash)
-        blue_mask[tissue_mask == 0] = 0
-        blue_palette = Palette.default_binary(rgb_color=[255, 179, 19])
-        layered_image_viewer.data.add_layer_or_modify_pixels(
-            BLUE_MASK_LAYER_NAME, blue_mask, FlatImage, blue_palette)
-
+    def _analyze_blue_ratio(self, layered_image_viewer: LayeredImageViewer, mask: np.ndarray):
         print('\n=== Initial Result ===')
-        blue_mask[tissue_mask == 0] = 0
-        blue_to_tissue_ratio = self._pixel_count_ratio(blue_mask, tissue_mask)
+        # Do not use: tissue_mask = mask != self._mask_background_class, because we can have some non tissue classes
+        is_tissue_mask = \
+            (mask == self._mask_foreground_class) \
+            | (mask == self._mask_blue_class) \
+            | (mask == self._mask_eroded_blue_class)
+
+        is_blue_mask = (mask == self._mask_blue_class) | (mask == self._mask_eroded_blue_class)
+        blue_to_tissue_ratio = self._pixel_count_ratio(is_blue_mask, is_tissue_mask)
 
         print('\n=== After Erode ===')
-        erode_kernel = np.ones((3, 3), np.uint8)
-        blue_mask = cv.erode(blue_mask, kernel=erode_kernel, iterations=1)
-        eroded_blue_to_tissue_ratio = self._pixel_count_ratio(blue_mask, tissue_mask)
-
-        blue_eroded_palette = Palette.default_binary(rgb_color=[255, 110, 19])
-        layered_image_viewer.data.add_layer_or_modify_pixels(
-            'blue-eroded_mask', blue_mask, FlatImage, blue_eroded_palette)
+        eroded_blue_to_tissue_ratio = self._pixel_count_ratio(mask == self._mask_eroded_blue_class, is_tissue_mask)
 
         ratio_message = f'Blue: {self._float_to_persent_str(blue_to_tissue_ratio)}' \
                         f'\nEroded Blue: {self._float_to_persent_str(eroded_blue_to_tissue_ratio)}'
@@ -199,12 +209,16 @@ class BiocellKidneyTissueSegmenter(QObject):
 
         tissue_palette = Palette.default_binary(rgb_color=[131, 151, 98])
         layered_image_viewer.data.add_layer_or_modify_pixels(
-            TISSUE_MASK_LAYER_NAME, tissue_mask, FlatImage, tissue_palette)
+            MASKS_LAYER_NAME, tissue_mask, FlatImage, tissue_palette)
 
     def _pixel_count_ratio(self, a: np.ndarray, b: np.ndarray) -> float:
+        if a.dtype == bool:
+            a = a.astype(np.uint8)
+        if b.dtype == bool:
+            b = b.astype(np.uint8)
         a_pixel_count = cv.countNonZero(a)
         b_pixel_count = cv.countNonZero(b)
-        a_to_b_ratio = a_pixel_count / b_pixel_count
+        a_to_b_ratio = a_pixel_count / b_pixel_count if b_pixel_count != 0 else 0
         print(f'a_pixel_count: {a_pixel_count}  b_pixel_count: {b_pixel_count}  ratio: {a_to_b_ratio}')
         return a_to_b_ratio
 

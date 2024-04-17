90e90c2a891e1686f5c05bc0d638f92ecf2aea32
Ivan Kosik <ivankosik91@gmail.com>
2024-04-17 12:11:45 +0300

    Fixed GUI tissue segmenter to remove inner small objects (not only external). Added different optimized methods to remove small objects and fill small holes.

--- biocell/src/bsmu/biocell/plugins/gui_tissue_segmenter.py
+++ biocell/src/bsmu/biocell/plugins/gui_tissue_segmenter.py
@@ -328,6 +328,8 @@ class TissueSegmenter(QObject):
     def segment(self, image: np.ndarray, config: TissueSegmentationConfig) -> np.ndarray:
         logging.info(f'Segment Tissue: {config}')
 
+        segmentation_start = timer()
+
         # Convert image into float, else cv.cvtColor returns np.uint8 and we will lose conversion precision
         image = np.float32(image) / 255
         hsb_image = cv.cvtColor(image, cv.COLOR_RGB2HSV)
@@ -354,11 +356,6 @@ class TissueSegmenter(QObject):
         if config.remove_small_object_size != 0:
             small_object_removing_start = timer()
 
-            # small_object_removing_kernel_size = (config.remove_small_object_size, config.remove_small_object_size)
-            # small_object_removing_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, small_object_removing_kernel_size)
-            # mask = self._mask_to_uint8(mask)
-            # mask = cv.morphologyEx(mask, cv.MORPH_OPEN, small_object_removing_kernel)
-
             mask = self._mask_to_uint8(mask)
             self._remove_small_objects(mask, config.remove_small_object_size)
 
@@ -367,27 +364,105 @@ class TissueSegmenter(QObject):
         if config.fill_hole_size != 0:
             holes_removing_start = timer()
 
-            fill_hole_kernel_size = (config.fill_hole_size, config.fill_hole_size)
-            fill_hole_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, fill_hole_kernel_size)
             mask = self._mask_to_uint8(mask)
-            mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, fill_hole_kernel)
+            self._fill_small_holes(mask, config.fill_hole_size)
 
-            logging.debug(f'Holes removing time: {timer() - holes_removing_start}')
+            logging.debug(f'Small hole filling time: {timer() - holes_removing_start}')
 
-        return self._mask_to_uint8(mask)
+        mask = self._mask_to_uint8(mask)
+        logging.debug(f'Tissue segmentation time: {timer() - segmentation_start}')
+        return mask
 
     @staticmethod
     def _mask_to_uint8(mask: np.ndarray) -> np.ndarray:
         return mask if mask.dtype == np.uint8 else mask.astype(np.uint8)
 
     @staticmethod
-    def _remove_small_objects(mask: np.ndarray, min_object_size: int):
+    def _remove_small_objects_and_holes_using_contours(
+            mask: np.ndarray,
+            min_object_size: int,
+            min_hole_size: int,
+            background_value: int = 0,
+            foreground_value: int = 1,
+    ):
+        # Find contours and hierarchy
+        contours, hierarchy = cv.findContours(mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+
+        # Create a list to hold the contours to be removed or filled
+        small_objects = []
+        small_holes = []
+
+        # Find small objects and holes
+        for i, contour in enumerate(contours):
+            area = cv.contourArea(contour)
+            # Check if the contour is an object (parent is -1)
+            if hierarchy[0][i][3] == -1:
+                if area < min_object_size:
+                    small_objects.append(contour)
+            # The contour is a hole
+            elif area < min_hole_size:
+                small_holes.append(contour)
+
+        # Remove small objects
+        cv.fillPoly(mask, small_objects, color=(background_value,))
+        # Fill small holes
+        cv.fillPoly(mask, small_holes, color=(foreground_value,))
+
+    @staticmethod
+    def _remove_small_external_objects(mask: np.ndarray, min_object_size: int, background_value: int = 0):
+        """ Removes only the extreme outer small objects. """
+        small_objects = []
         contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        # Filter out small objects
+        # Find small objects
         for contour in contours:
             if cv.contourArea(contour) < min_object_size:
-                # cv.drawContours(mask, [contour], contourIdx=-1, color=(0,), thickness=cv.FILLED)
-                cv.fillPoly(mask, [contour], color=(0,))
+                small_objects.append(contour)
+
+        cv.fillPoly(mask, small_objects, color=(background_value,))
+
+    @staticmethod
+    def _remove_small_objects_using_morphology(mask: np.ndarray, min_object_size: int):
+        small_object_removing_kernel_size = (min_object_size, min_object_size)
+        small_object_removing_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, small_object_removing_kernel_size)
+        cv.morphologyEx(mask, cv.MORPH_OPEN, small_object_removing_kernel, dst=mask)
+
+    @staticmethod
+    def _remove_small_objects(mask: np.ndarray, min_object_size: int, background_value: int = 0, connectivity: int = 8):
+        TissueSegmenter._modify_small_regions(
+            mask, min_object_size, background_value, connectivity, remove_small_objects=True)
+
+    @staticmethod
+    def _fill_small_holes_using_morphology(mask: np.ndarray, fill_hole_size: int):
+        fill_hole_kernel_size = (fill_hole_size, fill_hole_size)
+        fill_hole_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, fill_hole_kernel_size)
+        cv.morphologyEx(mask, cv.MORPH_CLOSE, fill_hole_kernel, dst=mask)
+
+    @staticmethod
+    def _fill_small_holes(mask: np.ndarray, min_hole_size: int, foreground_value: int = 1, connectivity: int = 8):
+        TissueSegmenter._modify_small_regions(
+            mask, min_hole_size, foreground_value, connectivity, remove_small_objects=False)
+
+    @staticmethod
+    def _modify_small_regions(
+            mask: np.ndarray,
+            min_region_size: int,
+            value_to_set: int,
+            connectivity: int = 8,
+            remove_small_objects: bool = True,
+    ):
+        """
+        :param remove_small_objects: True to remove small objects or False to fill small holes in the mask.
+        """
+        mask_to_analyze_connected_components = (
+            mask if remove_small_objects else 1 - mask  # invert the mask to find holes instead of objects
+        )
+        label_count, labels, stats, _ = cv.connectedComponentsWithStats(
+            mask_to_analyze_connected_components, connectivity=connectivity)
+        # Create a mask where labels of small regions are marked as True
+        skip_background = 1  # skip the first row, because it contains statistics of background
+        small_region_label_mask = stats[skip_background:, cv.CC_STAT_AREA] < min_region_size
+        # Set the pixels of small regions to `value_to_set`
+        mask[np.isin(labels, np.nonzero(small_region_label_mask)[0] + skip_background)] = value_to_set
 
     @staticmethod
     def _generate_corner_gradient(

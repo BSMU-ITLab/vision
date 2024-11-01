32688adcf18c95935236c29096df474df0f4ee21
Ivan Kosik <ivankosik91@gmail.com>
2024-11-01 21:20:34 +0300

    Fix(tools): Restore temporarily deactivated tool if mouse press or wheel used during fast shortcut release. Prevent restoring deactivated tool when shortcut key is typed in input widget

--- src/bsmu/vision/plugins/tools/viewer/manager.py
+++ src/bsmu/vision/plugins/tools/viewer/manager.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from PySide6.QtCore import Qt, QObject, QEvent, QElapsedTimer
+from PySide6.QtCore import Qt, QObject, QEvent, QElapsedTimer, QCoreApplication
 from PySide6.QtGui import QActionGroup
 
 from bsmu.vision.core.plugins.observer import ObserverPlugin
@@ -10,8 +10,8 @@ from bsmu.vision.plugins.tools.viewer import ViewerToolPlugin
 from bsmu.vision.plugins.windows.main import ToolsMenu
 
 if TYPE_CHECKING:
-    from bsmu.vision.plugins.windows.main import MainWindowPlugin, MainWindow
     from bsmu.vision.plugins.tools.viewer import MdiViewerTool
+    from bsmu.vision.plugins.windows.main import MainWindowPlugin, MainWindow
 
 
 class ViewerToolManagerPlugin(ObserverPlugin):
@@ -36,7 +36,9 @@ class ViewerToolManagerPlugin(ObserverPlugin):
         self._main_window.add_menu_action(
             ToolsMenu, self.tr('Uncheck Tool'), self._viewer_tool_manager.deactivate_active_tool, Qt.Key_Escape)
 
-        self._main_window.installEventFilter(self._viewer_tool_manager)
+        # We install the filter on app instance instead of the main window
+        # because the main window doesn't receive mouse press events from subwindow viewports
+        QCoreApplication.instance().installEventFilter(self._viewer_tool_manager)
 
     def _disable(self):
         self._main_window.removeEventFilter(self._viewer_tool_manager)
@@ -56,6 +58,8 @@ class ViewerToolManager(QObject):
     def __init__(self, main_window: MainWindow):
         super().__init__()
 
+        self._main_window = main_window
+
         self._tool_plugins: set[ViewerToolPlugin] = set()
         self._key_to_tool_plugin: dict[Qt.Key, ViewerToolPlugin] = {}
 
@@ -64,6 +68,7 @@ class ViewerToolManager(QObject):
 
         self._pressed_tool_action_shortcut_key: Qt.Key | None = None
         self._pressed_tool_action_shortcut_timer = QElapsedTimer()
+        self._is_mouse_used_during_tool_action_shortcut_being_pressed: bool = False
 
         self._active_viewer_tool: MdiViewerTool | None = None
         # Long press of a tool shortcut allows to temporarily activate the tool.
@@ -111,6 +116,12 @@ class ViewerToolManager(QObject):
             self._active_viewer_tool.deactivate()
 
     def eventFilter(self, watched_obj: QObject, event: QEvent):
+        if event.type() in (QEvent.MouseButtonPress, QEvent.Wheel):
+            self._is_mouse_used_during_tool_action_shortcut_being_pressed = True
+
+        if watched_obj != self._main_window:
+            return super().eventFilter(watched_obj, event)
+
         # We will not get QEvent.KeyPress events for action shortcuts, so use QEvent.ShortcutOverride instead.
         if event.type() == QEvent.ShortcutOverride and not event.isAutoRepeat():
             pressed_key = event.key()
@@ -124,17 +135,25 @@ class ViewerToolManager(QObject):
 
                 self._pressed_tool_action_shortcut_key = pressed_key
                 self._pressed_tool_action_shortcut_timer.start()
+                self._is_mouse_used_during_tool_action_shortcut_being_pressed = False
+
         elif (event.type() == QEvent.KeyRelease
               and event.key() == self._pressed_tool_action_shortcut_key
               and not event.isAutoRepeat()):
+
             pressing_time = self._pressed_tool_action_shortcut_timer.elapsed()
-            if pressing_time > 500:
+            if pressing_time > 300 or self._is_mouse_used_during_tool_action_shortcut_being_pressed:
                 # Activate previous temporary deactivated tool
                 if self._viewer_tool_temporary_deactivated is None:
                     self.deactivate_active_tool()
                 else:
                     self._viewer_tool_temporary_deactivated.activate()
-                    self._viewer_tool_temporary_deactivated = None
+
+            self._pressed_tool_action_shortcut_key = None
+            self._pressed_tool_action_shortcut_timer.invalidate()
+
+            self._viewer_tool_temporary_deactivated = None
+
         return super().eventFilter(watched_obj, event)
 
     def _on_viewer_tool_activating(self, viewer_tool: MdiViewerTool):
